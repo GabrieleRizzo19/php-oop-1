542281aa360e126a7b032b98875d20ba09a81049
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-06-20 16:02:47 +0200

    bonus 1

--- index.php
+++ index.php
@@ -5,27 +5,43 @@
         public $name;
         public $director;
         public $cast;
+        public $genres;
 
-        public function __construct($name, $director, $cast){
+        public function __construct($name, $director, $cast, $genres){
             
             $this->name = $name;
             $this->director = $director;
             $this->cast = $cast;
+            $this->genres = $genres;
         }
 
         public function infoToString(){
             $string = "<strong>Film:</strong> $this->name <strong>Regista:</strong> $this->director <strong>Cast:</strong> ";
+            
             foreach($this->cast as $actor){
-                $string .= "$actor ";
+                $string .= "$actor, ";
             }
+
+            $string .= "<strong>Genere:</strong> ";
+
+            if(is_array($this->genres)){
+                foreach($this->genres as $genre){
+                    $string .= "$genre, ";
+                }
+            }else{
+                $string .= "$this->genres";
+            }
+
+
             return $string;
         }
     }
 
-    $avengers = new Movie('The Avengers','Joss Whedon',['Robert Downey JR', 'Chris Evand']);
-    $spiderMan = new Movie('Spider-Man: Homecoming', 'Jon Watts', ['Tom Holland', 'Zendaya', 'Michael Keaton'])
+    $avengers = new Movie('The Avengers','Joss Whedon',['Robert Downey JR', 'Chris Evand'],['Heroes', 'Action']);
+    $spiderMan = new Movie('Spider-Man: Homecoming', 'Jon Watts', ['Tom Holland', 'Zendaya', 'Michael Keaton'], 'Heroes');
 
-
+    var_dump($avengers);
+    var_dump($spiderMan);
 
 ?>
 
